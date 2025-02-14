213d6a430d9e6f208e1619f66a116f06fcb7b010
LE Van Anh Khoa <106988219+AK087204@users.noreply.github.com>
2025-02-14 11:32:14 +0700

    add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+
+  // Cấu hình Swagger
+  const config = new DocumentBuilder()
+    .setTitle("My API")
+    .setDescription("API documentation for my project")
+    .setVersion("1.0")
+    .addTag("API")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  await app.listen(3000);
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
